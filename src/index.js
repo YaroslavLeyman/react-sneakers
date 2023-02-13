f1b31bcfa04b4b2a59ac06ad1b63422284ae8792
Iaroslav Leiman <yaroslavleiman@yandex.ru>
2023-02-13 13:51:56 +0700

    Корректная работа роутера

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 
 import './index.scss';
